6211808991b9344fb0f7a73433b904b2c0c3a766
Vector <qeemiaomiao@gmail.com>
2026-02-18 21:19:35 -0800

    Revert "Fix white-screen by deploying Vite build artifact to GitHub Pages"

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -14,9 +15,14 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
 )
